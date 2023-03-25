b72841635481b3269cb519f18131647300c6ae9f
Pakkerman <Pakkermandev.gmail.com>
2023-03-25 16:12:29 +0800

    Fix import path

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,14 @@ import { type NextPage } from "next"
 import Head from "next/head"
 import Link from "next/link"
 
+import { LoadingPage, LoadingSpinner } from "~/components/loading"
+
 import { api } from "~/utils/api"
 import type { RouterOutputs } from "~/utils/api"
 
 import dayjs from "dayjs"
 import relativeTime from "dayjs/plugin/relativeTime"
 import Image from "next/image"
-
-import { LoadingPage } from "~/components/Loading"
 
 dayjs.extend(relativeTime)
 
