7f69b848cda94df5808d2114a725b9a9f181377f
Pakkerman <Pakkermandev.gmail.com>
2023-03-30 18:46:15 +0800

    fix: clerkClient.getUserList().username is null, generate new username with first and last name

--- src/server/helpers/filterUserForClients.ts
+++ src/server/helpers/filterUserForClients.ts
@@ -1,9 +1,9 @@
 import { User } from "@clerk/nextjs/dist/api"
 export const filterUserForClient = (user: User) => {
-  console.log("ran filteruserforclient")
-  let username = user.username
-  if (username === null) username = user.firstName
-  if (user.lastName != null) username = username + user.lastName
+  let username =
+    user.username === null
+      ? `${user.firstName}${user?.lastName}`
+      : user.username
 
   return {
     id: user.id,
